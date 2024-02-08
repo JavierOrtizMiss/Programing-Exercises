ed0ab09221806251fb4804ce3a7d70d54ffea084
Javier de Jesus Ortiz Miss <javier.ortiz200512@gmail.com>
2024-02-08 11:22:56 -0600

    Update

--- C/Programación estructurada/Primo.c
+++ C/Programación estructurada/Primo.c
@@ -11,7 +11,7 @@ int main() {
     if (n == 1 || n == 0) {
         primo = 0;
     } else {
-        for (int i = 2; i <= n / 2; i++) {
+        for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) {
                 primo = 0;
                 break;
