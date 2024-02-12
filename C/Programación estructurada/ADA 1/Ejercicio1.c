84e33965c5ade405bd60084d278df1f00e98a6c2
Javier <javier.ortiz200512@gmail.com>
2024-02-12 15:42:00 -0600

    dd

--- C/Programación estructurada/ADA 1/Ejercicio1.c
+++ C/Programación estructurada/ADA 1/Ejercicio1.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 int main(){
+    int n1,n2,n3,mayor;
 
-    printf("Hola");
+    printf("Introduzca el primer número: ");
+    scanf("%d", &n1);
+    printf("Introduzca el segundo número: ");
+    scanf("%d", &n2);
+    printf("Introduzca el tercer número: ");
+    scanf("%d", &n3);
+
+    if (n1 > n2 && n1 > n3) {
+        mayor = n1;
+    } else if (n2 > n3) {
+        mayor = n2;
+    } else {
+        mayor = n3;
+    }
+
+    printf("El mayor de los tres números es: %d\n", mayor);
 
     return 0;
 }
