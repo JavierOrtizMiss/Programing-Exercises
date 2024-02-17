81371529493fd0e7c387c38b5b6d2442eb2362c8
Javier <javier.ortiz200512@gmail.com>
2024-02-16 20:35:05 -0600

    dasd

--- Python/POO/Parte2.py
+++ Python/POO/Parte2.py
@@ -1,8 +1,8 @@
 def f(w, x, y, z):
     return w and y and (not z) or (not w) and (not x) and y or x and (not y) and z or (not w) and y or x and (not z)
 
-def table1():
-    m = [[0 for _ in range(4)] for _ in range(16)]  # Matriz que almacena la tabla
+def elmejormaestrocuri():
+    m = [[0 for i in range(4)] for i in range(16)]  # Matriz que almacena la tabla
     n = 0  # Contador de las 16 combinaciones de booleanos
 
     # Ciclo que genera las combinaciones de valores booleanos
@@ -17,12 +17,64 @@ def table1():
                     n += 1
 
     # Ciclo para generar la tabla aplicando la función f
+    print("-----------------Tabla de verdad de la función-----------------")
+    print("[w][x][y][z] = [f(w,x,y,z)]")
     for n in range(16):
         result = f(m[n][0], m[n][1], m[n][2], m[n][3])  # Calculo de la función
         print("[%d][%d][%d][%d] = %d" % (m[n][0], m[n][1], m[n][2], m[n][3], result))
-        
-def tabledisyun():
-    return
     
+    print("--------------Funcion Booleana Disyuntiva-------------------")
+    for n in range(16):
+        if f(m[n][0], m[n][1], m[n][2], m[n][3]) == 1:
+            print("( ", end="")
+            if m[n][0] == 0:
+                print("w'", end=" ")
+            if m[n][0] == 1:
+                print("w", end=" ")
+            if m[n][1] == 0:
+                print("x'", end=" ")
+            if m[n][1] == 1:
+                print("x", end=" ")
+            if m[n][2] == 0:
+                print("y'", end=" ")
+            if m[n][2] == 1:
+                print("y", end=" ")
+            if m[n][3] == 0:
+                print("z'", end=" ")
+            if m[n][3] == 1:
+                print("z", end=" ")
+            print(") ", end="")
+            print(" + ", end="")
 
-table1()
+    print("\n\n\n--------------Funcion Booleana Conjuntiva-------------------")
+    for n in range(16):
+        if f(m[n][0], m[n][1], m[n][2], m[n][3]) == 0:
+            print("( ", end="")
+            if m[n][0] == 0:
+                print("w'", end=" ")
+            if m[n][0] == 1:
+                print("w", end=" ")
+            if m[n][1] == 0:
+                print("x'", end=" ")
+            if m[n][1] == 1:
+                print("x", end=" ")
+            if m[n][2] == 0:
+                print("y'", end=" ")
+            if m[n][2] == 1:
+                print("y", end=" ")
+            if m[n][3] == 0:
+                print("z'", end=" ")
+            if m[n][3] == 1:
+                print("z", end=" ")
+            print(") ", end="")
+            print(" + ", end="")
+    
+    # Mapa de Karnaugh
+    print("\n\n\n--------------Mapa de Karnaugh-------------------")
+    print("[wx]\\[yz]    00   01   11   10")
+    print("   00        %d    %d    %d    %d" % (f(0,0,0,0), f(0,0,0,1), f(0,0,1,1), f(0,0,1,0)))
+    print("   01        %d    %d    %d    %d" % (f(0,1,0,0), f(0,1,0,1), f(0,1,1,1), f(0,1,1,0)))
+    print("   11        %d    %d    %d    %d" % (f(1,1,0,0), f(1,1,0,1), f(1,1,1,1), f(1,1,1,0)))
+    print("   10        %d    %d    %d    %d" % (f(1,0,0,0), f(1,0,0,1), f(1,0,1,1), f(1,0,1,0)))
+
+elmejormaestrocuri()
--- Python/POO/tempCodeRunnerFile.py
+++ Python/POO/tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
 def f(w, x, y, z):
-    return w*y*(not z) + (not w)*(not x)*y + x*(not y)*z + (not w)*y + x*(not z)
+    return w and y and (not z) or (not w) and (not x) and y or x and (not y) and z or (not w) and y or x and (not z)
 
-def main():
-    m = [[0 for _ in range(4)] for _ in range(16)]  # Matriz que almacena la tabla
+def table1():
+    m = [[0 for i in range(4)] for i in range(16)]  # Matriz que almacena la tabla
     n = 0  # Contador de las 16 combinaciones de booleanos
 
     # Ciclo que genera las combinaciones de valores booleanos
@@ -17,9 +17,36 @@ def main():
                     n += 1
 
     # Ciclo para generar la tabla aplicando la función f
+    print("-----------------Tabla de verdad de la función-----------------")
     for n in range(16):
         result = f(m[n][0], m[n][1], m[n][2], m[n][3])  # Calculo de la función
         print("[%d][%d][%d][%d] = %d" % (m[n][0], m[n][1], m[n][2], m[n][3], result))
+    
+    print("--------------Funcion Booleana Disyuntiva-------------------")
+    for n in range(16):
+        if f(m[n][0], m[n][1], m[n][2], m[n][3]) == 1:
+            print("( ", end="")
+            if m[n][0] == 0:
+                print("w'", end=" ")
+            if m[n][0] == 1:
+                print("w", end=" ")
+            if m[n][1] == 0:
+                print("x'", end=" ")
+            if m[n][1] == 1:
+                print("x", end=" ")
+            if m[n][2] == 0:
+                print("y'", end=" ")
+            if m[n][2] == 1:
+                print("y", end=" ")
+            if m[n][3] == 0:
+                print("z'", end=" ")
+            if m[n][3] == 1:
+                print("z", end=" ")
+            print(") ", end="")
+            if n < 15:
+                print(" + ", end="")
+            else:
+                return
+    print()
 
-if __name__ == "__main__":
-    main()
+table1()
