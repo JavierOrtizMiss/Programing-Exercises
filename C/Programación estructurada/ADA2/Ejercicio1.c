efba8da0a8187f15f1d302bfd8119ad7572241b6
Javier <javier.ortiz200512@gmail.com>
2024-02-27 00:48:08 -0600

    Update

--- C/Programación estructurada/ADA2/Ejercicio1.c
+++ C/Programación estructurada/ADA2/Ejercicio1.c
@@ -1,19 +1,59 @@
+//Ortiz Miss Javier de Jesus
+
 #include <stdio.h>
 
+//Función para multiplicar
 int multiplicacion(float a, float b) {
     int resultado = 0;
     
-    for (int i = 0; i < (int)b; i++) {
+    for (int i = 0; i < (float)b; i++) {
         resultado += a;
     }
     return resultado;
 }
 
-int main() {
-    float a, b;
-    scanf("%f %f", &a, &b);
+// Función para calcular el número de Fibonacci dado su posición n
+int fibonacci(int n) {
+    if (n <= 1){
+        return n-1;
+    }
+    else if (n==2){
+        return n-1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
 
-    printf("%d\n", multiplicacion(a, b));
+// Función para calcular el factorial de un número
+int factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+// Función para calcular la potencia de un número
+int potencia(int base, int exponente) {
+    int resultado = 1;
+    for (int i = 0; i < exponente; i++) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
+int main() {
+    // Prueba de la función de multiplicación
+    printf("Multiplicación: %d\n", multiplicacion(5, 3));
+
+    // Prueba de la función de Fibonacci
+    printf("Fibonacci: %d\n", fibonacci(7));
+
+    // Prueba de la función de factorial
+    printf("Factorial: %d\n", factorial(5));
+
+    // Prueba de la función de potencia
+    printf("Potencia: %d\n", potencia(2, 3));
 
     return 0;
 }
