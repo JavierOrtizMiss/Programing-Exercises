906b11f855c789259dc4b836dfc06bbfe3df223e
Javier <javier.ortiz200512@gmail.com>
2024-03-07 13:01:24 -0600

    update

--- C/Programación estructurada/Proyecto/tictactoe.c
+++ C/Programación estructurada/Proyecto/tictactoe.c
@@ -2,28 +2,93 @@
 #include <unistd.h> //Librería para sleep
 int m[3][3];
 
-void tutorial(){
+void tutorial() {
 
-    printf("---------BIENVENIDO A TIC TAC TOE---------")
+    printf("---------BIENVENIDO A TIC TAC TOE---------\n\n");
 
-    printf("Tic Tac Toe, también conocido como tres en raya, es un juego de mesa para dos jugadores que se juega en una cuadrícula de 3x3. El objetivo del juego es colocar tres fichas del mismo tipo en línea, ya sea horizontal, vertical o diagonalmente, antes que el oponente. Cada jugador alterna colocando sus fichas en las casillas vacías hasta que uno de los jugadores logra formar una línea de tres fichas o se llena el tablero, resultando en un empate. ");
+    printf("Tic Tac Toe, también conocido como tres en raya, es un juego para dos\n"
+       "jugadores que se juega en una cuadrícula de 3x3. El objetivo del juego es colocar tres\n"
+       "fichas del mismo tipo en línea, ya sea horizontal, vertical o diagonalmente, antes que el oponente.\n"
+       "Cada jugador alterna colocando sus fichas en las casillas vacías hasta que uno de los jugadores logra\n"
+       "formar una línea de tres fichas o se llena el tablero, resultando en un empate.\n\n");
+    sleep(10);
 
-    printf("+-------+-------+-------+");
-    printf("|       |       |       |");
-    printf("|   1   |   2   |   3   |");
-    printf("|       |       |       |");
-    printf("+-------+-------+-------+");
-    printf("|       |       |       |");
-    printf("|   4   |   5   |   6   |");
-    printf("|       |       |       |");
-    printf("+-------+-------+-------+");
-    printf("|       |       |       |");
-    printf("|   7   |   8   |   9   |");
-    printf("|       |       |       |");
-    printf("+-------+-------+-------+");
+    printf("Un ejemplo de la cuadrícula es la siguiente:\n\n");
 
+    sleep(5);
 
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|   1   |   2   |   3   |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|   4   |   5   |   6   |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|   7   |   8   |   9   |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n\n");
 
+    sleep(5);
+
+    printf("Para realizar un movimiento, simplemente introduce el número correspondiente al cuadro donde deseas colocar tu ficha. \n"
+          "Cada número del 1 al 9 representa un cuadro del tablero. Por ejemplo:\n\n");
+
+    sleep(5);
+
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+
+    printf("\n");
+
+    printf("Introduce tu movimiento: 5\n\n");
+
+    sleep(5);
+
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |   X   |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n\n");
+
+    printf("Introduce tu movimiento: 1\n\n");
+
+    sleep(5);
+
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|   0   |   X   |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("|       |       |       |\n");
+    printf("+-------+-------+-------+\n");
 }
 
 void mostrar_cuadro(){
@@ -57,6 +122,8 @@ void introducir_movimiento(){
 
 
 int main () {
+
+    tutorial();
 
 
 
