6d9b08549a210f893cb9c2836fc8da91e130a5f9
Javier <javier.ortiz200512@gmail.com>
2024-05-14 21:33:44 -0600

    asd

--- C/Programación estructurada/Proyecto/tictactoe.c
+++ C/Programación estructurada/Proyecto/tictactoe.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h> // Para la función isdigit
 
 char m[3][3]; // Matriz para el tablero de juego
+int puntajes[2] = {0}; // Array para los puntajes de los jugadores
+char nombres[2][50]; // Array para los nombres de los jugadores
 
 void tutorial() {
     printf("---------BIENVENIDO A TIC TAC TOE---------\n\n");
@@ -63,20 +67,19 @@ int verificar_ganador(char jugador) {
     for (int i = 0; i < 3; i++) {
         if ((m[i][0] == jugador && m[i][1] == jugador && m[i][2] == jugador) ||
             (m[0][i] == jugador && m[1][i] == jugador && m[2][i] == jugador)) {
-            return 1; 
+            return 1;
         }
     }
 
     // Verificar si ganó en diagonal
     if ((m[0][0] == jugador && m[1][1] == jugador && m[2][2] == jugador) ||
         (m[0][2] == jugador && m[1][1] == jugador && m[2][0] == jugador)) {
-        return 1; 
+        return 1;
     }
     return 0; // No ha ganado
 }
 
 int tablero_lleno() {
-    
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (m[i][j] == ' ') {
@@ -87,12 +90,18 @@ int tablero_lleno() {
     return 1; // El tablero está lleno
 }
 
-void juegodos(){
+void juegodos() {
     int jugador1, jugador2;
     char respuesta;
+    int valid_input; // Variable para verificar la entrada válida
 
     printf("¡Que empiece el juego!\n");
     printf("Jugador 1 es X y jugador 2 es O\n");
+
+    printf("Ingrese el nombre del Jugador 1: ");
+    scanf("%s", nombres[0]);
+    printf("Ingrese el nombre del Jugador 2: ");
+    scanf("%s", nombres[1]);
 
     do {
         // Inicializar el tablero
@@ -104,55 +113,65 @@ void juegodos(){
 
         do {
             mostrar_cuadro();
-            printf("Jugador 1 introduce tu movimiento: ");
-            scanf("%d", &jugador1);
-            if (jugador1>0 && jugador1<10)
-            {
+            valid_input = 0; // Inicializar la variable a 0
+            printf("%s introduce tu movimiento: ", nombres[0]);
+            if (scanf("%d", &jugador1) == 1 && jugador1 > 0 && jugador1 < 10) {
+                valid_input = 1; // La entrada es válida
                 m[(jugador1 - 1) / 3][(jugador1 - 1) % 3] = 'X';
-            if (verificar_ganador('X')) {
+                if (verificar_ganador('X')) {
+                    mostrar_cuadro();
+                    printf("¡Felicidades! %s ha ganado.\n", nombres[0]);
+                    puntajes[0] += 20;
+                    break;
+                }
+
+                if (tablero_lleno()) {
+                    printf("El tablero está lleno. ¡Es un empate!\n");
+                    break;
+                }
+            } else {
+                printf("Por favor, introduzca un número válido\n");
+                // Limpiar el búfer de entrada
+                while (getchar() != '\n');
+            }
+
+            if (valid_input) {
                 mostrar_cuadro();
-                printf("¡Felicidades! Jugador 1 (X) ha ganado.\n");
-                break;
+                valid_input = 0; // Reiniciar la variable a 0
+                printf("%s introduce tu movimiento: ", nombres[1]);
+                if (scanf("%d", &jugador2) == 1 && jugador2 > 0 && jugador2 < 10) {
+                    valid_input = 1; // La entrada es válida
+                    m[(jugador2 - 1) / 3][(jugador2 - 1) % 3] = 'O';
+                    if (verificar_ganador('O')) {
+                        mostrar_cuadro();
+                        printf("¡Felicidades! %s ha ganado.\n", nombres[1]);
+                        puntajes[1] += 20;
+                        break;
+                    }
+
+                    if (tablero_lleno()) {
+                        printf("El tablero está lleno. ¡Es un empate!\n");
+                        break;
+                    }
+                } else {
+                    printf("Por favor, introduzca un número válido\n");
+                    // Limpiar el búfer de entrada
+                    while (getchar() != '\n');
+                }
             }
 
-            if (tablero_lleno()) {
-                printf("El tablero está lleno. ¡Es un empate!\n");
-                break;
-            }
-            } else
-            {
-                printf("Por favor, introduzca un número valido\n");
-            }
-            
-        
-
-            mostrar_cuadro();
-            printf("Jugador 2 introduce tu movimiento: ");
-            scanf("%d", &jugador2);
-            if (jugador2>0 && jugador2<10)
-            {
-                m[(jugador2 - 1) / 3][(jugador2 - 1) % 3] = 'O';
-            if (verificar_ganador('O')) {
-                mostrar_cuadro();
-                printf("¡Felicidades! Jugador 2 (O) ha ganado.\n");
-                break;
-            }
-
-            if (tablero_lleno()) {
-                printf("El tablero está lleno. ¡Es un empate!\n");
-                break;
-            }
-            } else
-            {
-                printf("Por favor, introduzca un número valido\n");
-            }
-            
-            
         } while (1);
 
         printf("¿Deseas jugar de nuevo? (s/n): ");
         scanf(" %c", &respuesta);
     } while (respuesta == 's');
+}
+
+void ver_puntajes() {
+    printf("Puntaje actual:\n");
+    for (int i = 0; i < 2; i++) {
+        printf("%s: %d puntos\n", nombres[i], puntajes[i]);
+    }
 }
 
 int main() {
@@ -163,8 +182,8 @@ int main() {
     while (opcion != 0) {
         printf("Seleccione una opción:\n\n");
         printf("1. Ver tutorial\n"
-               "2. Jugar con la máquina\n"
-               "3. Jugar con otro jugador\n"
+               "2. Jugar con otro jugador\n"
+               "3. Ver puntajes\n"
                "0. Salir\n\n");
         printf("Opción: ");
         scanf("%d", &opcion);
@@ -174,10 +193,10 @@ int main() {
                 tutorial();
                 break;
             case 2:
-                //jugarmaquina();
+                juegodos();
                 break;
             case 3:
-                juegodos();
+                ver_puntajes();
                 break;
             case 0:
                 printf("------EL JUEGO HA FINALIZADO----\n");
