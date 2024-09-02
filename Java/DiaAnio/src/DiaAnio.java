7bfcca1ff8597e37edd264b6fef3b7401ac0ba27
Javier <javier.ortiz200512@gmail.com>
2024-09-01 20:27:06 -0600

    asd

--- Java/DiaAnio/src/DiaAnio.java
+++ Java/DiaAnio/src/DiaAnio.java
@@ -1,5 +1,35 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class DiaAnio {
+
+    // Atributos
+    private int mes;
+    private int dia;
+    private int anio;
+
+    // Constructor
+    public DiaAnio(int mes, int dia, int anio){
+        this.mes = mes;
+        this.dia = dia; 
+        this.anio = anio;
+    }
+
+    // Getters
+    public int getMes(){
+        return mes;
+    }
+
+    public int getDia(){
+        return dia;
+    }
+
+    public int getAnio(){
+        return anio;
+    }
+
+    public boolean igual(int mes, int dia, int anio){
+        return this.mes == mes && this.dia == dia && this.anio == anio;
+    }
+
+    public void visualizar(){
+        System.out.println(dia + "/" + mes + "/" + anio);
     }
 }
