5738d385eb4e1f5d879e197c26d0eeb4b11d8c7d
Javier <javier.ortiz200512@gmail.com>
2024-09-02 09:01:36 -0600

    Asd

--- Java/ADA2/src/Cuenta.java
+++ Java/ADA2/src/Cuenta.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+/**
+ * Cuenta
+ */
+public class Cuenta {
+
+    private Double saldo;
+    private String nocuenta;
+    private String description;
+
+    public Cuenta(Double saldo, String nocuenta, String description){
+        this.saldo = saldo;
+        this.nocuenta = nocuenta;
+        this.description = description;
     }
+
+    public Double getSaldo(){
+        return saldo;
+    }
+    public String getNocuenta(){
+        return nocuenta;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    //hola como estas
+    
 }
