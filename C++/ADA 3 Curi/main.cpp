b12d3169f9a60d67f60043fe666628a37af57bf3
JavierOrtizMiss <javier.ortiz200512@gmail.com>
2024-09-23 22:23:46 -0600

    ASd

--- C++/ADA 3 Curi/main.cpp
+++ C++/ADA 3 Curi/main.cpp
@@ -5,16 +5,7 @@ using namespace std;
 
 int main(){
     ifstream archivo("automata.txt");
-    string line;
-
-    while (getline(archivo,line))
-    {
-        cout << line << endl;
-    }
-    
-
-
-
-    archivo.close();
     return 0;
 }
+
+
