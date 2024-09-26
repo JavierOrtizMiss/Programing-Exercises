b2fd9066494459b83e3d90e0299e5c90b60325b4
Javier <javier.ortiz200512@gmail.com>
2024-09-25 19:24:51 -0600

    ASD

--- Java/Cuadrilatero/src/Cuadrilatero.java
+++ Java/Cuadrilatero/src/Cuadrilatero.java
@@ -1,5 +1,11 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Cuadrilatero {
+    private int[][] vertices = new int[4][2];
+    
+    public Cuadrilatero(int[][]vertices){
+        this.vertices = vertices;
     }
+
+    public int[][] getVertice(){
+        return vertices;
+}
 }
