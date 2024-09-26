406bbe0a49438148d1f775ec6f7f2b74aa2f73ac
Javier <javier.ortiz200512@gmail.com>
2024-09-25 19:24:51 -0600

    ASD

---
+++ Java/Cuadrilatero/src/Trapecio.java
@@ -0,0 +1,19 @@
+public class Trapecio {
+    private int bMayor;
+    private int bMenor;
+
+    public Trapecio(int bMayor, int bMenor){
+        this.bMayor = bMayor;
+        this.bMenor = bMenor;
+    }
+    
+    public int getBMayor(){
+        
+        return bMayor;
+    }
+
+    public int getBmenor(){
+        return bMenor;
+    }
+
+}
