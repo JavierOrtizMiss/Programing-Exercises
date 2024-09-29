192609393172f34463a100c2e9459eadb9b94305
Javier <javier.ortiz200512@gmail.com>
2024-09-28 19:00:38 -0600

    ASD

--- Java/ADA Curi/src/Automaton/Automaton.java
+++ Java/ADA Curi/src/Automaton/Automaton.java
@@ -1,10 +1,22 @@
 package Automaton;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Set;
+
 
 public class Automaton {
     private String stateInitial;
     private Set<String> statesFinal;
     private Set<String> alphabet;
+    privatec
+
+
+public void readAutomaton(){
+    
+    
+
+}
     
 }
