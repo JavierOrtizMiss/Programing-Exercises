bf764229a4acb9cc9136ba22eb9d63f722c570a4
Javier <javier.ortiz200512@gmail.com>
2024-09-30 17:31:22 -0600

    ASD

--- Java/ADA Curi/src/Automaton/Automaton.java
+++ Java/ADA Curi/src/Automaton/Automaton.java
@@ -1,6 +1,6 @@
-// src/automata/Automaton.java
+//Importamos la carpeta "Automaton"
 package Automaton;
-
+//Declaración de las librerías
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,30 +10,34 @@ import java.util.List;
 import java.util.Set;
 import States.States;
 
+//Creamos la clase Automaton que hará todo el procedimiento de leer el automata y validarlo
 public class Automaton {
     private List<States> estados; // Lista de estados
     private String estadoInicial; // Estado inicial
     private Set<String> estadosFinales; // Conjunto de estados finales
     private Set<String> alfabeto; // Alfabeto del autómata
 
+    //Creamos un constructor de la clase donde creamos objetos de cada apartado del autómata, al igual que cargar el archivo
     public Automaton(String archivo) throws IOException {
-        estados = new ArrayList<>();
-        estadosFinales = new HashSet<>();
-        alfabeto = new HashSet<>();
-        cargarAutomata(archivo);
+        estados = new ArrayList<>();//Creamos el objeto lista
+        estadosFinales = new HashSet<>();//Creamos el objeto set o conjunto
+        alfabeto = new HashSet<>();//Creamos el objeto set o conjunto
+        cargarAutomata(archivo);//Llamamos al método cargarAutómata que tiene como parámetro el archivo
+        //Con throws vemos si hay alguna excepcion al cargar el archivo
     }
 
     // Método para cargar el autómata desde un archivo
     private void cargarAutomata(String archivo) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(archivo));
-        String linea;
-        String seccionActual = "";
+        BufferedReader br = new BufferedReader(new FileReader(archivo)); //Instanciamos el método br de BufferReader para poder leer el archivo
+        String linea;//Declaramos la variable linea de tipo String, nos servirá para poder leer linea por linea
+        String seccionActual = "";//Creamos una variable seccionActual de tipo String y le agregamos una cadena vacía
 
+        //Ciclo while para poder ir leyendo linea 
         while ((linea = br.readLine()) != null) {
-            linea = linea.trim();
-            if (linea.isEmpty()) continue;
+            linea = linea.trim();//Quitamos espacios en blanco de la cadena si es que llegara a tener
+            if (linea.isEmpty()) continue;//Si la cadena es vacía, pasa a la siguiente
 
-            if (linea.startsWith("#")) {
+            if (linea.startsWith("#")) {//Si empieza con # extrae esa seccion
                 seccionActual = linea.substring(1); // Extraer sección sin el #
                 continue;
             }
@@ -53,7 +57,7 @@ public class Automaton {
                     break;
                 case "transitions":
                     // Separar la línea de transición en partes
-                    String[] partes = linea.split(">");
+                    String[] partes = linea.split(">");//Dividir la cadena en partes con el caracter ">"
                     String[] estadoYSimbolo = partes[0].split(":");
                     String estadoDesde = estadoYSimbolo[0];
                     String simbolo = estadoYSimbolo[1];
