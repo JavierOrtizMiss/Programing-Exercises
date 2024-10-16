9496effe224fda7dbad6a4ce44f86d4ddec2e55e
Javier <javier.ortiz200512@gmail.com>
2024-10-15 22:05:43 -0600

    ASD

---
+++ Java/InterfacesGráficas/src/MiGui.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class MiGui {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame();
+        ventana.setTitle("Mi programa");
+        ventana.setSize(300,500);
+        ventana.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+    
+}
